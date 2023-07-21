37a95e555797d665a3f6c7ea365d0864f87799ea
Ilan Ponsky <iponsky97@gmail.com>
2023-07-21 10:35:53 -0400

    Upload react app

---
+++ ilans-burgers-inc/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Main from './Main';
+import Services from './Services';
+import About from './About';
+import Pricing from './Pricing';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* <Main /> */}
+      {/* <Services /> */}
+      {/* <About /> */}
+      {/* <Pricing /> */}
+      {/* <Contact /> */}
+    </div>
+  );
+}
+
+export default App;
